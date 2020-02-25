7a5cad753304f4b3d3a73682a58d9d6dd4fff762
William L <w-lindvall@outlook.com>
2020-02-25 00:13:45 -0800

    fix notes render on server error

--- client/js/screens/Notes.js
+++ client/js/screens/Notes.js
@@ -6,28 +6,22 @@ import {ScrollView} from 'react-native-gesture-handler'
 import {FEED} from '../context/apollo'
 
 const Notes = () => {
-  const {
-    loading,
-    error,
-    data: {feed},
-  } = useQuery(FEED)
+  const {loading, error, data} = useQuery(FEED)
 
   return (
     <SafeAreaView>
       <ScrollView>
         <Text>notes page</Text>
 
-        {error ? (
+        {error && (
           <Text>
             Something went wrong. Please restart the App to try again.
           </Text>
-        ) : null}
-
-        {loading ? (
-          <Text>Loading...</Text>
-        ) : (
-          <Text>{feed.map(d => d.id)}</Text>
         )}
+
+        {loading && <Text>Loading...</Text>}
+
+        {!error && data && <Text>{data.feed.map(d => d.id)}</Text>}
       </ScrollView>
     </SafeAreaView>
   )
