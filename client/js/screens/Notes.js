d84330593c25e56c8ec45e88bd194105d32d2f5f
William L <w-lindvall@outlook.com>
2020-02-24 21:10:14 -0800

    render notes list

--- client/js/screens/Notes.js
+++ client/js/screens/Notes.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import {useQuery} from '@apollo/react-hooks'
 import {Text} from 'react-native'
 import {SafeAreaView} from 'react-native'
 import {ScrollView} from 'react-native-gesture-handler'
+import {FEED} from '../context/apollo'
 
 const Notes = () => {
+  const {
+    loading,
+    error,
+    data: {feed},
+  } = useQuery(FEED)
+
   return (
     <SafeAreaView>
       <ScrollView>
         <Text>notes page</Text>
+
+        {error ? (
+          <Text>
+            Something went wrong. Please restart the App to try again.
+          </Text>
+        ) : null}
+
+        {loading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <Text>{feed.map(d => d.id)}</Text>
+        )}
       </ScrollView>
     </SafeAreaView>
   )
