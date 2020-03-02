4a4f7e72ab6caa28e992e9c5ce950f5676533845
William L <w-lindvall@outlook.com>
2020-03-01 18:13:26 -0800

    name gql queries

--- client/js/context/apollo/queries.js
+++ client/js/context/apollo/queries.js
@@ -1,7 +1,7 @@
 import {gql} from 'apollo-boost'
 
 const INBOX = gql`
-  {
+  query inbox {
     inbox {
       id
       message
@@ -10,7 +10,7 @@ const INBOX = gql`
 `
 
 const OUTBOX = gql`
-  {
+  query outbox {
     outbox {
       id
       message
