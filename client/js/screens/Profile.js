f05f3e480d60b1a45089133bafedf412d9faea77
graziela silva <grazims26@gmail.com>
2020-03-03 01:36:11 -0800

    add style to profile page

--- client/js/screens/Profile.js
+++ client/js/screens/Profile.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import {SafeAreaView} from 'react-native'
-import {ScrollView} from 'react-native-gesture-handler'
 import {Header} from '../components/Typography'
-import {TagsInput, TagsList} from '../components'
+import {TagsInput, TagsList, styles} from '../components'
 
 const Profile = () => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        {/* add avatar */}
-        <Header>Topics of Interest</Header>
-        <TagsInput placeholder="Write your topic" />
-        <TagsList />
-        <Header>Favourite Notes</Header>
-        {/* add favs notes */}
-      </ScrollView>
+    <SafeAreaView style={styles.noteArea}>
+      {/* add avatar */}
+      {/* add name */}
+      <Header>Topics of Interest</Header>
+      <TagsInput placeholder="Write your topic" />
+      <TagsList />
+      <Header>Favourite Notes</Header>
+      {/* add favs notes */}
     </SafeAreaView>
   )
 }
