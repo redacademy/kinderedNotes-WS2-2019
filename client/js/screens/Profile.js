268afdd66eed9b662d6150a6edfafaa55bee55cd
graziela silva <grazims26@gmail.com>
2020-03-03 01:22:23 -0800

    add components to profile page

--- client/js/screens/Profile.js
+++ client/js/screens/Profile.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import {Text} from 'react-native'
 import {SafeAreaView} from 'react-native'
 import {ScrollView} from 'react-native-gesture-handler'
+import {Header} from '../components/Typography'
+import {TagsInput, TagsList} from '../components'
 
 const Profile = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>profile page</Text>
+        {/* add avatar */}
+        <Header>Topics of Interest</Header>
+        <TagsInput placeholder="Write your topic" />
+        <TagsList />
+        <Header>Favourite Notes</Header>
+        {/* add favs notes */}
       </ScrollView>
     </SafeAreaView>
   )
