61a5060df7b51f17368d8ad1eef7716be2e1d2a1
Roberto H <robhhr@outlook.com>
2020-03-09 01:55:17 -0700

    persist network requests on android

--- client/js/context/apollo/client.js
+++ client/js/context/apollo/client.js
@@ -1,11 +1,14 @@
 import {ApolloClient} from 'apollo-client'
+import {Platform} from 'react-native'
 import {InMemoryCache} from 'apollo-cache-inmemory'
 import {createHttpLink} from 'apollo-link-http'
 import {setContext} from 'apollo-link-context'
 import AsyncStorage from '@react-native-community/async-storage'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000',
+  uri: `http://${
+    Platform.OS === 'ios' ? 'localhost:4000' : '10.0.0.184:4000'
+  }`,
 })
 
 const authLink = setContext(async (_, {headers}) => {
