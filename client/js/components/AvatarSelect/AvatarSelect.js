15d012c7b028dc28e06f389389a3522c7a86e51e
William L <w-lindvall@outlook.com>
2020-03-08 19:20:32 -0700

    stagger avatar select options animations

--- client/js/components/AvatarSelect/AvatarSelect.js
+++ client/js/components/AvatarSelect/AvatarSelect.js
@@ -9,20 +9,19 @@ import {
 import styles from './AvatarSelect.styles'
 import AvatarOption from './AvatarOption'
 
+const AVATAR_AMOUNT = 6
+
 const AvatarSelect = ({currentAvatar, onChange}) => {
-  const [avatarOffetAnim] = useState(new Animated.Value(0))
-  const [avatarOptionOpacity] = useState(new Animated.Value(0))
+  const [avatarAnimVals] = useState(
+    Array.from({length: AVATAR_AMOUNT}, () => new Animated.Value(0)),
+  )
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleIsOpen = () => {
-    Animated.timing(avatarOffetAnim, {
-      toValue: isOpen ? 0 : 1,
-      duration: 400,
-    }).start()
-    Animated.timing(avatarOptionOpacity, {
-      toValue: isOpen ? 0 : 1,
-      duration: 400,
-    }).start()
+    const avatarAnimations = avatarAnimVals.map(anim =>
+      Animated.timing(anim, {toValue: isOpen ? 0 : 1, duration: 300}),
+    )
+    Animated.stagger(27.5, avatarAnimations).start()
 
     setIsOpen(s => !s)
   }
@@ -32,20 +31,36 @@ const AvatarSelect = ({currentAvatar, onChange}) => {
     toggleIsOpen()
   }
 
-  const avatarOffet = avatarOffetAnim.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0rem', '100rem'],
-  })
+  const avatarOffsets = avatarAnimVals.map(anim =>
+    anim.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0rem', '100rem'],
+    }),
+  )
 
   return (
     <View style={styles.container}>
       <AvatarOption
         onPress={handleAvatarSelect}
         style={{
-          opacity: avatarOptionOpacity,
+          opacity: avatarAnimVals[0],
           transform: [
+            {scale: avatarAnimVals[0]},
+            {rotate: '325deg'},
+            {translateX: avatarOffsets[0]},
+            {rotate: '-325deg'},
+          ],
+        }}
+        source={require('../../../assets/images/memoji-6.jpg')}
+      />
+      <AvatarOption
+        onPress={handleAvatarSelect}
+        style={{
+          opacity: avatarAnimVals[1],
+          transform: [
+            {scale: avatarAnimVals[1]},
             {rotate: '15deg'},
-            {translateX: avatarOffet},
+            {translateX: avatarOffsets[1]},
             {rotate: '-15deg'},
           ],
         }}
@@ -54,10 +69,11 @@ const AvatarSelect = ({currentAvatar, onChange}) => {
       <AvatarOption
         onPress={handleAvatarSelect}
         style={{
-          opacity: avatarOptionOpacity,
+          opacity: avatarAnimVals[2],
           transform: [
+            {scale: avatarAnimVals[2]},
             {rotate: '65deg'},
-            {translateX: avatarOffet},
+            {translateX: avatarOffsets[2]},
             {rotate: '-65deg'},
           ],
         }}
@@ -66,10 +82,11 @@ const AvatarSelect = ({currentAvatar, onChange}) => {
       <AvatarOption
         onPress={handleAvatarSelect}
         style={{
-          opacity: avatarOptionOpacity,
+          opacity: avatarAnimVals[3],
           transform: [
+            {scale: avatarAnimVals[3]},
             {rotate: '115deg'},
-            {translateX: avatarOffet},
+            {translateX: avatarOffsets[3]},
             {rotate: '-115deg'},
           ],
         }}
@@ -78,10 +95,11 @@ const AvatarSelect = ({currentAvatar, onChange}) => {
       <AvatarOption
         onPress={handleAvatarSelect}
         style={{
-          opacity: avatarOptionOpacity,
+          opacity: avatarAnimVals[4],
           transform: [
+            {scale: avatarAnimVals[4]},
             {rotate: '165deg'},
-            {translateX: avatarOffet},
+            {translateX: avatarOffsets[4]},
             {rotate: '-165deg'},
           ],
         }}
@@ -90,26 +108,15 @@ const AvatarSelect = ({currentAvatar, onChange}) => {
       <AvatarOption
         onPress={handleAvatarSelect}
         style={{
-          opacity: avatarOptionOpacity,
+          opacity: avatarAnimVals[5],
           transform: [
+            {scale: avatarAnimVals[5]},
             {rotate: '215deg'},
-            {translateX: avatarOffet},
+            {translateX: avatarOffsets[5]},
             {rotate: '-215deg'},
           ],
         }}
         source={require('../../../assets/images/memoji-5.jpg')}
-      />
-      <AvatarOption
-        onPress={handleAvatarSelect}
-        style={{
-          opacity: avatarOptionOpacity,
-          transform: [
-            {rotate: '325deg'},
-            {translateX: avatarOffet},
-            {rotate: '-325deg'},
-          ],
-        }}
-        source={require('../../../assets/images/memoji-6.jpg')}
       />
 
       <TouchableOpacity
