c6c204b5335447c8ff384aaf225e62fa5a96b484
William L <w-lindvall@outlook.com>
2020-03-10 18:49:13 -0700

    add terms and conditions to signup flow

--- client/js/components/CreateUserForm/CreateUserForm.js
+++ client/js/components/CreateUserForm/CreateUserForm.js
@@ -1,15 +1,95 @@
-import React, {useState} from 'react'
-import {SafeAreaView, Button} from 'react-native'
-import {ScrollView} from 'react-native-gesture-handler'
+import React, {useState, useEffect} from 'react'
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Text,
+} from 'react-native'
 import {Header} from '../Typography'
 import {
   AvatarSelect,
   TagsInput,
   TagsList,
   Input,
-  styles,
+  Button,
+  styles as globalStyles,
 } from '../index'
 import {useAuth} from '../../hooks'
+import Animated, {Easing} from 'react-native-reanimated'
+import termsContent from './terms-text'
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+  },
+  profileContainer: {},
+  termsContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    width: '100%',
+    backgroundColor: '#fff',
+  },
+  termsHeader: {
+    marginVertical: 23,
+  },
+  termsText: {
+    lineHeight: 27,
+    marginBottom: 100,
+  },
+})
+
+const Terms = ({onComplete}) => {
+  const [anim] = useState(new Animated.Value(0))
+
+  useEffect(() => {
+    Animated.timing(anim, {
+      toValue: 1,
+      duration: 400,
+      easing: Easing.quad,
+    }).start()
+  }, [])
+
+  const onAccept = () => {
+    Animated.timing(anim, {
+      toValue: 0,
+      duration: 400,
+      easing: Easing.quad,
+    }).start()
+
+    onComplete()
+  }
+
+  const offset = anim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [700, 0],
+  })
+
+  return (
+    <Animated.View
+      style={{
+        marginTop: offset,
+        opacity: anim,
+        ...styles.termsContainer,
+      }}
+    >
+      <ScrollView style={styles.profileContainer}>
+        <Header style={styles.termsHeader}>Terms & Conditions</Header>
+        <Text style={styles.termsText}>{termsContent}</Text>
+      </ScrollView>
+      <Button
+        style={{
+          zIndex: 999,
+          position: 'absolute',
+          bottom: 0,
+        }}
+        onPress={onAccept}
+      >
+        Accept
+      </Button>
+    </Animated.View>
+  )
+}
 
 const CreateUserForm = ({authData}) => {
   const {signup} = useAuth()
@@ -17,7 +97,11 @@ const CreateUserForm = ({authData}) => {
   const [cityInput, setCityInput] = useState('')
   const [currentAvatar, setCurrentAvatar] = useState(0)
   const [interestsInput, setInterestsInput] = useState([])
+  const [hasAcceptedTerms, setHasAcceptedTerms] = useState(false)
+  const [termsOpen, setTermsOpen] = useState(false)
 
+  const acceptTerms = () => setHasAcceptedTerms(true)
+  const openTerms = () => setTermsOpen(true)
   const createUser = () => {
     signup({
       variables: {
@@ -31,7 +115,9 @@ const CreateUserForm = ({authData}) => {
   }
 
   return (
-    <SafeAreaView style={styles.noteArea}>
+    <SafeAreaView
+      style={{...globalStyles.noteArea, ...styles.container}}
+    >
       <ScrollView>
         <Header>Profile Information</Header>
         <AvatarSelect
@@ -55,8 +141,15 @@ const CreateUserForm = ({authData}) => {
           placeholder="Anxiety"
         />
         <TagsList tags={interestsInput} setTags={setInterestsInput} />
-        <Button onPress={createUser} title="Get Started" />
       </ScrollView>
+
+      {hasAcceptedTerms ? (
+        <Button onPress={createUser}>Get Started</Button>
+      ) : (
+        <Button onPress={openTerms}>Terms and conditions</Button>
+      )}
+
+      {termsOpen && <Terms onComplete={acceptTerms} />}
     </SafeAreaView>
   )
 }
