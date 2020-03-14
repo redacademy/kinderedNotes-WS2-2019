c33923fe6ab477acdf0944115753596f412c8a8f
William L <w-lindvall@outlook.com>
2020-03-13 18:54:29 -0700

    hook up client note response input

--- client/js/components/ReceivedNote/ReceivedNote.js
+++ client/js/components/ReceivedNote/ReceivedNote.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   SafeAreaView,
   View,
@@ -8,9 +8,25 @@ import {
 import {useActiveNote} from '../../hooks'
 import {Bookmark, Input} from '../index'
 import styles from './ReceivedNote.styles'
+import {useMutation} from '@apollo/react-hooks'
+import {CREATE_NOTE_RESPONSE} from '../../context'
 
 const ReceivedNote = () => {
+  const [createNoteResponse] = useMutation(CREATE_NOTE_RESPONSE, {
+    refetchQueries: ['inbox'],
+  })
+  const [replyInput, setReplyInput] = useState('')
   const {activeNote} = useActiveNote()
+
+  const onReplySubmit = () => {
+    createNoteResponse({
+      variables: {
+        id: activeNote.id,
+        message: replyInput,
+      },
+    })
+    setReplyInput('')
+  }
 
   return (
     <SafeAreaView style={styles.receivedContainer}>
@@ -19,8 +35,15 @@ const ReceivedNote = () => {
           <Text style={styles.font}>{activeNote.message}</Text>
           <Bookmark />
         </View>
-        <Input placeholder={'Thanks for this note.'} />
-        <TouchableOpacity style={styles.replyButton}>
+        <Input
+          value={replyInput}
+          onChangeText={setReplyInput}
+          placeholder={'Thanks for this note.'}
+        />
+        <TouchableOpacity
+          onPress={onReplySubmit}
+          style={styles.replyButton}
+        >
           <Text style={styles.reply}>Reply</Text>
         </TouchableOpacity>
       </View>
