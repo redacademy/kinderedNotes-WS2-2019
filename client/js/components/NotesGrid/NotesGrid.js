646f13b1ebeeebb1963d8816654f28344abb3872
William L <w-lindvall@outlook.com>
2020-03-16 16:38:26 -0700

    add loading indicator to notes grid

--- client/js/components/NotesGrid/NotesGrid.js
+++ client/js/components/NotesGrid/NotesGrid.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {View, Text, FlatList} from 'react-native'
-import NotesGridItem from './NotesGridItem'
-import {COLORS} from '../styles'
-import styles from './NotesGrid.styles'
+import {View, Text, FlatList, ActivityIndicator} from 'react-native'
 import {useActiveNote} from '../../hooks'
+import {COLORS} from '../styles'
+import NotesGridItem from './NotesGridItem'
+import styles from './NotesGrid.styles'
 
 const NotesGrid = ({data, loading, error, Icon, onNotePress, bg}) => {
   const {setActiveNote} = useActiveNote()
@@ -20,46 +20,46 @@ const NotesGrid = ({data, loading, error, Icon, onNotePress, bg}) => {
     )
   }
 
-  if (loading) {
-    return <Text>Loading notes...</Text>
-  }
-
   return (
     <View style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={({item}) => (
-          <NotesGridItem
-            color={bg || item.color}
-            style={item.style}
-            onPress={() => handleNotePress(item)}
-          >
-            {Icon ? (
-              <Icon width="95" />
-            ) : (
-              <Text
-                style={{
-                  ...styles.itemText,
-                  fontFamily:
-                    item.font === 'DEFAULT'
-                      ? 'Nunito-SemiBold'
-                      : item.font === 'HANDWRITTEN'
-                      ? 'Playlist-Script'
-                      : 'PermanentMarker',
-                  color:
-                    item.style === 'FILL'
-                      ? COLORS.TEXT_PRIMARY.INVERT
-                      : COLORS[`ACCENT_${item.color}`],
-                }}
-              >
-                {item.message}
-              </Text>
-            )}
-          </NotesGridItem>
-        )}
-        numColumns={3}
-        keyExtractor={(_, i) => i.toString()}
-      />
+      {loading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({item}) => (
+            <NotesGridItem
+              color={bg || item.color}
+              style={item.style}
+              onPress={() => handleNotePress(item)}
+            >
+              {Icon ? (
+                <Icon width="95" />
+              ) : (
+                <Text
+                  style={{
+                    ...styles.itemText,
+                    fontFamily:
+                      item.font === 'DEFAULT'
+                        ? 'Nunito-SemiBold'
+                        : item.font === 'HANDWRITTEN'
+                        ? 'Playlist-Script'
+                        : 'PermanentMarker',
+                    color:
+                      item.style === 'FILL'
+                        ? COLORS.TEXT_PRIMARY.INVERT
+                        : COLORS[`ACCENT_${item.color}`],
+                  }}
+                >
+                  {item.message}
+                </Text>
+              )}
+            </NotesGridItem>
+          )}
+          numColumns={3}
+          keyExtractor={(_, i) => i.toString()}
+        />
+      )}
     </View>
   )
 }
