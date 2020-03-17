7ad702aaac99cd2db0d26ab6b54f4a3de9543a79
William L <w-lindvall@outlook.com>
2020-03-16 20:55:29 -0700

    persist auth state

--- client/js/context/auth.js
+++ client/js/context/auth.js
@@ -3,7 +3,7 @@ import React, {createContext, useState} from 'react'
 const AuthContext = createContext({user: null, setUser: null})
 
 export const AuthContextProvider = ({children}) => {
-  const [user, setUser] = useState()
+  const [user, setUser] = useState(undefined)
 
   return (
     <AuthContext.Provider value={{user, setUser}}>
