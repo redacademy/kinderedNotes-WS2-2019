7b559f0c6ba9f5add83648e461c8ef49e778f7ef
Roberto H <robhhr@outlook.com>
2020-03-18 21:30:28 -0700

    show custom note bookmark

--- client/js/components/Bookmark/Bookmark.js
+++ client/js/components/Bookmark/Bookmark.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import {TouchableOpacity} from 'react-native'
+import {useActiveNote} from '../../hooks'
 import BookmarkIcon from '../../../assets/icons/Bookmark.svg'
+import BookmarkBlue from '../../../assets/icons/BookmarkBlue.svg'
 import BookmarkIconFill from '../../../assets/icons/Bookmark_filled.svg'
+import BlueBookmarkIconFill from '../../../assets/icons/Bookmark_filledBlue.svg'
 import styles from './Bookmark.styles'
 
-const Bookmark = ({onPress, filled = false}) => (
-  <TouchableOpacity style={styles.bookmark} onPress={onPress}>
-    {filled ? <BookmarkIconFill /> : <BookmarkIcon />}
-  </TouchableOpacity>
-)
+const Bookmark = ({onPress, filled = false}) => {
+  const {activeNote} = useActiveNote()
+  return (
+    <TouchableOpacity style={styles.bookmark} onPress={onPress}>
+      {activeNote.style === 'BORDERED' ? (
+        filled ? (
+          <BlueBookmarkIconFill />
+        ) : (
+          <BookmarkBlue />
+        )
+      ) : filled ? (
+        <BookmarkIconFill />
+      ) : (
+        <BookmarkIcon />
+      )}
+    </TouchableOpacity>
+  )
+}
 
 export default Bookmark
